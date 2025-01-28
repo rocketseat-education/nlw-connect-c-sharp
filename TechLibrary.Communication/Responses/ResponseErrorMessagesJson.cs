236bb012b02fbe9a09135939c4c0e350cafa0c6a
Lara Azevedo <lara.azevedo@outlook.com>
2025-01-28 14:03:05 -0300

    Aula 01 - Iniciando o projeto

---
+++ TechLibrary.Communication/Responses/ResponseErrorMessagesJson.cs
@@ -0,0 +1,6 @@
+﻿namespace TechLibrary.Communication.Responses;
+
+public class ResponseErrorMessagesJson
+{
+    public List<string> Errors { get; set; } = [];
+}
---
+++ TechLibrary.Exception/TechLibraryException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception;
+
+public abstract class TechLibraryException : SystemException
+{
+    public abstract List<string> GetErrorMessages();
+    public abstract HttpStatusCode GetStatusCode();
+} 
